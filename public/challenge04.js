b458e6da83bca7e347a8a4a39c89e0fc8af7ebd4
Rebeca Baruch <71520936+RebecaBaruch@users.noreply.github.com>
2022-11-03 19:41:46 -0300

    Update challenge04.js

--- public/challenge04.js
+++ public/challenge04.js
@@ -1,7 +1,12 @@
+let texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
+let queries = ["a","em","i","el"];
+let k = 2;
+
 function calculaTopOcorrenciasDeQueries(texto,queries,k) {
-    let totalT = [];
+    let objTerm = [];
+    let totalT = []
     let oco = [];
-    let ocQueries = [];
+  
     
     //enquanto for menor que o número de queries...
     for(i=0;i<queries.length; i++){
@@ -9,28 +14,31 @@ function calculaTopOcorrenciasDeQueries(texto,queries,k) {
        let termos = (texto.split(queries[i])).length-1;
   
       //coloca como objeto a querie e o número de ocorrências dela
-       totalT.push({query: queries[i] ,
+       objTerm.push({query: queries[i] ,
                            termo: termos});
+       totalT.push(termos);
     }
     
     //enquanto j for menor que k -> número solicitado pelo 'usuário'
     for(j=0;j<k; j++){
         //percorre o atributo 'termo' de cada objeto na arraym e guarda em 'terT'
-        let terT = totalT.map(object => {
+        let terT = objTerm.map(object => {
           return object.termo;
         });
       
-        //verifica e armazena em 'max' o maior termo em totalT, que agora está armazenado em terT
+        //verifica e armazena em 'max' o maior termo em objTerm, que agora está armazenado em terT
         let max = Math.max(...terT);
-        let maxIndex = totalT.map(object => object.terT).indexOf(max); //pega a index do maior valor
+        let maxIndex = objTerm.map(object => object.terT).indexOf(max); //pega a index do maior valor
       
         console.log(maxIndex);
   
     }
    
-    for(n=0; n<totalT.length;n++){
-      console.log(totalT[n].query, totalT[n].termo);
+    for(n=0; n<objTerm.length;n++){
+      console.log(objTerm[n].query, objTerm[n].termo);
     }
   
     console.log(oco);
   }
+
+  calculaTopOcorrenciasDeQueries(texto,queries,k);
