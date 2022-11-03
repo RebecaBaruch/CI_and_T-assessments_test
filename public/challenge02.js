3de6159f0be2491716c1e28630ac303da1355db3
Rebeca Baruch <71520936+RebecaBaruch@users.noreply.github.com>
2022-11-03 20:46:07 -0300

    atualizacao

--- public/challenge02.js
+++ public/challenge02.js
@@ -1,20 +1,24 @@
 function retornaMenorEMaiorValorDeVendas(tickets) {
-
-    let vendas = []
-    let resultado = []
-   
-    //for alinhado para percorrer a matriz
-     for(l=0;l<tickets.length;l++){
-       for(c=0;c<tickets[l].length;c++){
-         //vai verificar se os valores estão entre 20 e 500 para adicionar
-         if(tickets[l][c] > 20 &&  tickets[l][c] < 500) {
-         vendas.push(tickets[l][c])
-       }
-     }
-   } 
-     //pegando o menor e o maior valor e adicionando no array
-     let min = resultado.push(Math.min.apply(null, vendas))
-     let max = resultado.push(Math.max.apply(null, vendas))
-   
-     console.log(resultado)
-   }
+  
+    //declara viráveis de venda e resultado
+    let sales = [];
+    let resultVal = [];
+  
+   //percorre a matriz
+    for(row=0;row<tickets.length;row++){
+      for(col=0;col<tickets[row].length;col++){
+        
+        //verifica se os valores estão entre 20 e 500
+        if((tickets[row][col]>=20) && (tickets[row][col]<=500)) {
+          //se responde às condições, adiciona a sales
+         sales.push(tickets[row][col]);
+      }
+    }
+  } 
+    //pega o maior e menor valor para add a array
+    let minVal = resultVal.push(Math.min.apply(null, sales));
+    let maxVal = resultVal.push(Math.max.apply(null, sales));
+  
+    return resultVal;
+  }
+  
--- public/challenge04.js
+++ public/challenge04.js
@@ -1,44 +1,37 @@
-let texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
-let queries = ["a","em","i","el"];
-let k = 2;
-
 function calculaTopOcorrenciasDeQueries(texto,queries,k) {
-    let objTerm = [];
-    let totalT = []
-    let oco = [];
+  let objTerm = [];
+  let totalT = []
+  let oco = [];
+  let resultOco = [];
   
+  //enquanto for menor que o número de queries...
+  for(i=0;i<queries.length; i++){
+     //verifica o número de ocorrências de cada querie
+     let termos = (texto.split(queries[i])).length-1;
+
+    //coloca como objeto a querie e o número de ocorrências dela
+     objTerm.push({query: queries[i] ,
+                  termo: termos});
+     totalT.push(termos);
+  }
+  
+  //enquanto j for menor que k -> número solicitado pelo 'usuário'   
+  for(j=0;j<k; j++){
+      let max = Math.max.apply(null, totalT);
+      let maxIndex = totalT.indexOf(max);
     
-    //enquanto for menor que o número de queries...
-    for(i=0;i<queries.length; i++){
-       //verifica o número de ocorrências de cada querie
-       let termos = (texto.split(queries[i])).length-1;
-  
-      //coloca como objeto a querie e o número de ocorrências dela
-       objTerm.push({query: queries[i] ,
-                           termo: termos});
-       totalT.push(termos);
-    }
-    
-    //enquanto j for menor que k -> número solicitado pelo 'usuário'
-    for(j=0;j<k; j++){
-        //percorre o atributo 'termo' de cada objeto na arraym e guarda em 'terT'
-        let terT = objTerm.map(object => {
-          return object.termo;
-        });
+      if(objTerm[maxIndex].termo = max){
+          oco.push(objTerm[maxIndex]);
+      }
       
-        //verifica e armazena em 'max' o maior termo em objTerm, que agora está armazenado em terT
-        let max = Math.max(...terT);
-        let maxIndex = objTerm.map(object => object.terT).indexOf(max); //pega a index do maior valor
-      
-        console.log(maxIndex);
-  
-    }
-   
-    for(n=0; n<objTerm.length;n++){
-      console.log(objTerm[n].query, objTerm[n].termo);
-    }
-  
-    console.log(oco);
+      totalT.splice(maxIndex, 1);
+
   }
 
-  calculaTopOcorrenciasDeQueries(texto,queries,k);
+  for (var n = 0; n< oco.length; n++) {
+      resultOco.push(oco[n].query);
+  }
+  
+  console.log(resultOco);
+  return resultOco;
+}
